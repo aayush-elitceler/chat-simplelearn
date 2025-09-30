efa57e944514489e3500fe8376bdd5a0d0fec898
aayush-elitceler <aayush.sharma@elitceler.com>
2025-09-30 18:38:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://user.selflearnai.in",
     ],
     allow_credentials=True,
     allow_methods=["*"],
