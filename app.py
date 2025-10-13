c3aa9075a70d2a09b6e08e970f52c7b0308fd66b
aayush-elitceler <aayush.sharma@elitceler.com>
2025-10-13 12:33:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://user.selflearnai.in",
+        "https://sadhana.selflearnai.in",
     ],
     allow_credentials=True,
     allow_methods=["*"],
